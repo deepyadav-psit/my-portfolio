7fa7b9c0d9406385c5442160a0a10d7814707334
Deep yadav <deepyadav8421@gmail.com>
2025-09-09 12:25:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple form handler (prevent empty submission)
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+});
+
+
